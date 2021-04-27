b5f6d483800493dcd39b56ce2194b1ad9469ed41
Joshua Siby <joshua.siby@claysys.com>
2021-04-27 17:41:50 +0530

    Second commit

--- HelloWorld/src/main/java/com/example/main/Hello.java
+++ HelloWorld/src/main/java/com/example/main/Hello.java
@@ -2,6 +2,6 @@ package com.example.main;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Initial code");
+        System.out.println("This belongs to second commit");
     }
 }
